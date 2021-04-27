2d473b0abacd1b2272b9867a5059321da4e9d11b
imrathiii <195567@nith.ac.in>
2021-04-27 19:34:55 +0530

    mentor form link and sponsorship form link added

---
+++ js/timer.js
@@ -0,0 +1,54 @@
+const year = new Date().getFullYear();
+const fourthOfJuly = new Date(year, 6,4).getTime();
+const fourthOfJulyNextYear = new Date(year + 1, 6, 4).getTime();
+const month = new Date().getMonth();
+
+// countdown
+let timer = setInterval(function() {
+
+  // get today's date
+  const today = new Date().getTime();
+
+  // get the difference
+  let diff;
+  if(month > 6) {
+    diff = fourthOfJulyNextYear - today;
+  } else {
+    diff = fourthOfJuly - today;
+  }
+
+
+
+
+  // math
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  if(seconds<10){
+    seconds = "0"+seconds;
+  }
+  if(minutes<10){
+    minutes = "0"+minutes;
+  }
+  if(hours<10){
+    hours = "0"+hours;
+  }
+  if(days<10){
+    days = "0"+days;
+  }
+
+  // display
+  document.getElementById("timer").innerHTML =
+    `<div class="days"> 
+  <div class="numbers"> ${days} </div>days</div> 
+<div class="hours"> 
+  <div class="numbers"> ${hours} </div>hours</div> 
+<div class="minutes"> 
+  <div class="numbers"> ${minutes} </div>minutes</div> 
+<div class="seconds"> 
+  <div class="numbers"> ${seconds} </div>seconds</div> 
+</div>`;
+
+}, 1000);
